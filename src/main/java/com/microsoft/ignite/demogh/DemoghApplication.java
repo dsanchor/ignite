e1f4daa3e95ff4f68b270cea38eded0615e1b43a
dsanchor <sancheski@gmail.com>
2024-02-25 19:30:25 +0100

    first commit

---
+++ src/main/java/com/microsoft/ignite/demogh/DemoghApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.ignite.demogh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microsoft/ignite/demogh/DemoghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.ignite.demogh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
